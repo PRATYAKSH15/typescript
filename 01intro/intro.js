1c2f39fc372f09c04c3694c0ae175982e3963a2b
Pratyaksh <pratyaksh1594@gmail.com>
2025-09-01 14:38:54 +0530

    initial commit

---
+++ 01intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Alice", age: 30 };
+console.log("Hello, welcome to the TypeScript introduction!");
+console.log(user.email);
---
+++ 01intro/intro.ts
@@ -0,0 +1,3 @@
+const user = {name: "Alice", age: 30};
+console.log("Hello, welcome to the TypeScript introduction!");
+console.log(user.name);
