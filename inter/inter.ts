b4e341b85f2eef642a7b697b8972d8b6fcad7a2e
Pratyaksh <pratyaksh1594@gmail.com>
2025-09-16 23:51:43 +0530

    added sub

--- inter/inter.ts
+++ inter/inter.ts
@@ -5,12 +5,18 @@ function add(a: number, b: number): number {
   return a + b;
 }
 
+// Subtraction function
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
 // Variables with types
 let x: number = 10;
 let y: number = 20;
 
-// Call the function
+// Call the functions
 console.log("Sum:", add(x, y));
+console.log("Difference:", subtract(x, y));
 
 // Example of string type
 let message: string = "Hello TypeScript!";
